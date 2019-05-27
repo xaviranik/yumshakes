5a5dbd24d53dc18d740077dfbc6128819b01ac1b
Xavir <xaviranik.me@gmail.com>
2019-05-27 19:36:28 +0600

    Project init

---
+++ src/components/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="index">
+    <div class="container">
+      <div class="card">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      shakes: [
+        {
+          id: 1,
+          title: 'Ninja Shake',
+          slug: 'ninja-shake',
+          ingredients: ['banana', 'coffee', 'milk']
+        },
+        {
+          id: 2,
+          title: 'Good Game Coffee Played',
+          slug: 'good-game-coffee-played',
+          ingredients: ['strawberry', 'coffee', 'hazelnut', 'oreo']
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+   <div class="navbar">
+     <nav class="nav-extended purple darken-1">
+       <div class="nav-content">
+          <router-link to=""><span class="nav-title">YumShakes</span></router-link>
+            <a href="" class="btn-floating btn-large halfway-fab waves-effect waves-light pink">
+              <router-link to="">
+                <i class="material-icons">add</i>
+              </router-link>
+            </a>
+       </div>
+     </nav>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+.navbar nav {
+  padding: 0 20px;
+}
+</style>
